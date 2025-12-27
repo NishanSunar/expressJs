011dd4ad6bc4896ea61741260216b4297590743e
NishanSunar <barailinishant908@gmail.com>
2025-12-27 22:07:40 +0545

    using use() to implement 404 error page

--- Html_rendering/index.js
+++ Html_rendering/index.js
@@ -1,5 +1,5 @@
 import express from 'express'
-import path from 'path'
+import path, { dirname } from 'path'
 const app = express()
 const __dirname = path.resolve()
 
@@ -12,6 +12,10 @@ app.get("/login",(req, resp)=>{
 })
 app.post("/submit",(req, resp)=>{
     resp.send("<h1>Data submitted</h1>");
+})
+
+app.use((req, resp)=>{
+    resp.status(404).sendFile(path.join(__dirname,"html_rendering/404.html"))
 })
 
 app.listen(5600)
